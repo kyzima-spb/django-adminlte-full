c66f9a0df03597bc7c882779ab249a83c0e19e13
Kirill Vercetti <office@kyzima-spb.com>
2016-04-18 18:44:16 +0300

    Added assets paths [temp]

---
+++ adminlte_full/assets.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+ASSETS = {
+    'bootstrap_css': 'AdminLTE/bootstrap/css/bootstrap.min.css',
+    'bootstrap_js': 'AdminLTE/bootstrap/js/bootstrap.min.js',
+
+    'seiyria-bootstrap-slider_css': 'AdminLTE/plugins/slider.css',
+    'seiyria-bootstrap-slider_js': 'AdminLTE/plugins/bootstrap-slider.js',
+
+    'bootstrap3-wysihtml5-bower_css': 'AdminLTE/plugins/bootstrap3-wysihtml5.min.css',
+    'bootstrap3-wysihtml5-bower_js': 'AdminLTE/plugins/bootstrap3-wysihtml5.min.js',
+
+    'Chart.js_js': 'AdminLTE/plugins/chartjs/Chart.min.js',
+
+    'ckeditor_js': 'AdminLTE/plugins/ckeditor/ckeditor.js',
+
+    'mjolnic-bootstrap-colorpicker_css': 'AdminLTE/plugins/bootstrap-colorpicker.min.css',
+    'mjolnic-bootstrap-colorpicker_js': 'AdminLTE/plugins/bootstrap-colorpicker.min.js',
+
+    'datatables_css': 'AdminLTE/plugins/datatables/jquery.dataTables.min.css',
+    'datatables_js': 'AdminLTE/plugins/datatables/jquery.dataTables.min.js',
+
+    'datatables_css': 'AdminLTE/plugins/datatables/dataTables.bootstrap.css',
+    'datatables_js': 'AdminLTE/plugins/datatables/dataTables.bootstrap.js',
+
+    'bootstrap-datepicker_css': 'AdminLTE/plugins/datepicker3.css',
+    'bootstrap-datepicker_js': 'AdminLTE/plugins/bootstrap-datepicker.js',
+
+    'jquery': 'AdminLTE/plugins/jQuery/jQuery-2.2.0.min.js',
+
+    'html5shiv': '~3.7.2',
+    'respond': '~1.4.2',
+    'components-font-awesome': '~4.5',
+    'ionicons': '*',
+
+    'bootstrap-daterangepicker': '~1.3.21',
+    'fastclick': '1.0.6',
+    'flot': '~0.8.3',
+    'fullcalendar': '~2.3.2',
+    'iCheck': '~1.0.2',
+    'jquery.inputmask': '~3.1.63',
+    'ion.rangeSlider': '~2.0.9',
+    'jquery-ui': '~1.11.4',
+    'bower-jvectormap': '~1.2.2',
+    'jquery-knob': '~1.2.11',
+    'morris.js': '~0.5.1',
+    'raphael': '~2.1.4',
+    'pace': '~1.0.2',
+    'slimScroll': '~1.3.3',
+    'jqueryui-timepicker-addon': '~1.5.5'
+}
