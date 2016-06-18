4548b98201bf99eb30f64ec0831b6efde2ea3713
Kirill Vercetti <office@kyzima-spb.com>
2016-06-18 23:58:00 +0300

    Added default empty views

--- adminlte_full/views.py
+++ adminlte_full/views.py
@@ -1,4 +1,42 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.contrib import messages
+
+
+class BaseView(TemplateView):
+    __instance = None
+
+    @classmethod
+    def replace_with(cls, instance):
+        cls.__instance = instance
+
+    @classmethod
+    def instance(cls):
+        return cls.__instance or cls
+
+
+class MessageView(BaseView):
+    template_name = 'adminlte_full/base-layout.html'
+
+    def get(self, request, *args, **kwargs):
+        messages.debug(request, 'Redefine this page')
+        return render(request, self.template_name)
+
+
+class NotificationView(BaseView):
+    template_name = 'adminlte_full/base-layout.html'
+
+    def get(self, request, *args, **kwargs):
+        messages.debug(request, 'Redefine this page')
+        return render(request, self.template_name)
+
+
+class TaskView(BaseView):
+    template_name = 'adminlte_full/base-layout.html'
+
+    def get(self, request, *args, **kwargs):
+        messages.debug(request, 'Redefine this page')
+        return render(request, self.template_name)
 
 
 def index(request):
