91d6c70098dfd0128bca5e7cfeeac5ac97c707f5
Kirill Vercetti <office@kyzima-spb.com>
2016-06-19 14:46:17 +0300

    Now work "Change password" and "Logout"

--- adminlte_full/urls.py
+++ adminlte_full/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    url(r'', include('django.contrib.auth.urls')),
+
     url(r'^message/(?P<id>\w+)$', views.index, name='adminlte_full_show_message'),
     url(r'^messages$', views.index, name='adminlte_full_all_messages'),
 
@@ -11,6 +14,22 @@ urlpatterns = [
     url(r'^task/(?P<id>\w+)$', views.index, name='adminlte_full_show_task'),
     url(r'^tasks$', views.index, name='adminlte_full_all_tasks'),
 
-    url(r'^profile$', views.index, name='adminlte_full_profile'),
-    url(r'^logout$', views.index, name='adminlte_full_logout'),
+    url(
+        r'^profile$',
+        auth_views.password_change,
+        {
+            'template_name': 'adminlte_full/user/password_change_form.html',
+            'post_change_redirect': 'adminlte_full_profile'
+        },
+        name='adminlte_full_profile'
+    ),
+
+    url(
+        r'^logout$',
+        auth_views.logout_then_login,
+        {
+            'login_url': '/'
+        },
+        name='adminlte_full_logout'
+    ),
 ]
