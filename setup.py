7449fedccd86aa73dc487affb07ea64bb5b7cc5d
Kirill Vercetti <office@kyzima-spb.com>
2020-04-25 18:53:47 +0300

    Automatic generation of the package version.

--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
-import adminlte_full
 
-
-version = adminlte_full.__version__
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
+
 setup(
     name='django-adminlte-full',
-    version=version,
-    packages=['adminlte_full'],
-    install_requires=[
-        'django>=1.9',
-        'django-bootstrap3>=7.0'
-    ],
-    include_package_data=True,
-    license='Apache License 2.0',
+    use_scm_version=True,
+    url='https://github.com/kyzima-spb/django-adminlte-full',
     description='This Django application is port the AdminLTE Template for easy integration into Django Framework',
     long_description=README,
-    url='https://github.com/kyzima-spb/django-adminlte-full',
     author='Kirill Vercetti',
     author_email='office@kyzima-spb.com',
+    license='Apache License 2.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'setuptools_scm>=3.5',
+        'django>=3.0',
+        'django-bootstrap3>=7.0'
+    ],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
