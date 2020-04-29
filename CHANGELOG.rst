e03df95ebb226b35cb5d121b99dd8b82e1969d3f
Kirill Vercetti <office@kyzima-spb.com>
2020-04-29 16:32:08 +0300

    Added CHANGELOG and updated setup.py

---
+++ CHANGELOG.rst
@@ -0,0 +1,9 @@
+0.2.0
+=====
+
+Upgrading to Bootstrap 4 and AdminLTE 3.
+
+0.1.1
+=====
+
+The release was added only to maintain backward compatibility. All new code will no longer work with the old version.
--- setup.py
+++ setup.py
@@ -1,26 +1,35 @@
-from setuptools import setup, find_packages
 import os
 
+from setuptools import setup, find_packages
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
+
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        return f.read()
+
+
+readme = read_file('README.rst')
+changelog = read_file('CHANGELOG.rst')
 
 
 setup(
     name='django-adminlte-full',
-    use_scm_version=True,
+    use_scm_version={
+        'relative_to': __file__,
+    },
     url='https://github.com/kyzima-spb/django-adminlte-full',
     description='This Django application is port the AdminLTE Template for easy integration into Django Framework',
-    long_description=README,
+    long_description=readme + '\n\n' + changelog,
+    long_description_content_type='text/x-rst',
     author='Kirill Vercetti',
     author_email='office@kyzima-spb.com',
     license='Apache License 2.0',
     packages=find_packages(),
     include_package_data=True,
+    setup_requires=['setuptools_scm'],
     install_requires=[
-        'setuptools_scm>=3.5',
         'django>=3.0',
-        'django-bootstrap3>=7.0'
+        'django-bootstrap4>=1.0'
     ],
     classifiers=[
         'Environment :: Web Environment',
