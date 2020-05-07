01c2e6a37b79c514922c47444961f0bf2eec2c2f
Kirill Vercetti <office@kyzima-spb.com>
2020-05-07 20:30:08 +0300

    Рефакторинг меню

--- adminlte_full/models.py
+++ adminlte_full/models.py
@@ -1,12 +1,11 @@
 """
 Add icons to the links using the .nav-icon class with font-awesome or any other icon font library
 """
-from adminlte_base import MenuItemmixin, MenuMixin
-from adminlte_base.menu import MenuItem
+from adminlte_base import MenuItemMixin, MenuMixin, MenuItem
 from django.db import models
 
 
-class MenuItemModel(models.Model, MenuItemmixin):
+class MenuItemModel(models.Model, MenuItemMixin):
     TYPES = (
         (MenuItem.TYPE_LINK, MenuItem.TYPE_LINK.title()),
         (MenuItem.TYPE_HEADER, MenuItem.TYPE_HEADER.title()),
@@ -19,11 +18,11 @@ class MenuItemModel(models.Model, MenuItemmixin):
     type = models.CharField(
         max_length=20, default=MenuItem.TYPE_LINK, choices=TYPES
     )
+    title = models.CharField(max_length=500)
     url = models.URLField(max_length=500, blank=True)
-    endpoint = models.CharField(max_length=255)
+    endpoint = models.CharField(max_length=255, blank=True)
     endpoint_args = models.TextField(blank=True)
     endpoint_kwargs = models.TextField(blank=True)
-    title = models.CharField(max_length=500)
     icon = models.CharField(max_length=50, blank=True)
     help = models.CharField(max_length=500, blank=True)
     pos = models.IntegerField(default=0, blank=True)
@@ -38,7 +37,7 @@ class MenuModel(models.Model, MenuMixin):
 
     @property
     def items(self):
-        return self.menuitemmodel_set.select_related('parent').order_by('parent', 'pos').all()
+        return self.menuitemmodel_set.select_related('parent')
 
     def __str__(self):
         return f'{self.title} [{self.program_name}]'
