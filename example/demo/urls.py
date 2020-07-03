b7249682dffc34e1073e918facdf0d879fdc33f6
Kirill Vercetti <office@kyzima-spb.com>
2020-07-03 23:29:03 +0300

    Update example

--- example/demo/urls.py
+++ example/demo/urls.py
@@ -6,6 +6,7 @@ from adminlte_full.adminlte import config
 
 urlpatterns = [
     path('', views.index),
+    path('', views.index, name=config['ADMINLTE_REGISTRATION_ENDPOINT']),
     path('terms/', views.terms),
     path('profile/', views.profile, name=config['ADMINLTE_PROFILE_ENDPOINT']),
 ]
